e5d9e4300bf806c2dec9490f7307f66a4601efa4
ricknoboa137 <45580543+ricknoboa137@users.noreply.github.com>
2024-06-17 21:21:43 +0200

    file uploaded

---
+++ Sensorfusion.ino
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ * See the bottom of this file for the license terms.
+ */
+
+/*
+   This sketch example demonstrates how the BMI160 on the
+   Intel(R) Curie(TM) module can be used to read gyroscope data
+*/
+
+#include "CurieIMU.h"
+#include "SensorFusion.h" //SF
+SF fusion;
+
+float gx, gy, gz, ax, ay, az, mx, my, mz;
+float pitch, roll, yaw;
+float deltat;
+
+void setup() {
+  Serial.begin(115200); // initialize Serial communication
+  while (!Serial);    // wait for the serial port to open
+
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+
+  // Set the accelerometer range to 250 degrees/second
+  CurieIMU.setGyroRange(250);
+  CurieIMU.setAccelerometerRange(2);
+}
+
+void loop() {
+  //float gx, gy, gz; //scaled Gyro values
+
+  // read gyro measurements from device, scaled to the configured range
+  CurieIMU.readGyroScaled(gx, gy, gz);
+  gx = gx * DEG_TO_RAD;
+  gy = gy * DEG_TO_RAD;
+  gz = gz * DEG_TO_RAD;
+  CurieIMU.readAccelerometerScaled(ax, ay, az);
+  // display tab-separated gyro x/y/z values
+
+  deltat = fusion.deltatUpdate(); //this have to be done before calling the fusion update
+  //choose only one of these two:
+  fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
+  //fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //else use the magwick, it is slower but more accurate
+
+  pitch = fusion.getPitch();
+  roll = fusion.getRoll();    //you could also use getRollRadians() ecc
+  yaw = fusion.getYaw();
+
+  Serial.print("Pitch:"); Serial.print(pitch); Serial.print(",");
+  Serial.print("Roll:"); Serial.print(roll); Serial.print(",");
+  Serial.print("Yaw:"); Serial.println(yaw);
+  //Serial.println();
+}
+
+
+/*
+   Copyright (c) 2016 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
